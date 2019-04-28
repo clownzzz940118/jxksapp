ee93c6c02d3384fcabf42cf4d67574474e705703
dsm <365628463@qq.com>
2019-04-28 17:58:22 +0800

    用户登陆

--- script/cookie.js
+++ script/cookie.js
@@ -1,26 +1,26 @@
-function addCookie(name,value,expireHours){
-             var cookieString=name+"="+escape(value);
-             //判断是否设置过期时间
-             if(expireHours>0){
-                    var date=new Date();
-                    date.setTime(date.getTime+expireHours*3600*1000);
-                    cookieString=cookieString+"; expire="+date.toGMTString();
-             }
-             document.cookie=cookieString;
+function addCookie(name, value, expireHours) {
+    var cookieString = name + "=" + escape(value);
+    //判断是否设置过期时间
+    if (expireHours > 0) {
+        var date = new Date();
+        date.setTime(date.getTime + expireHours * 3600 * 1000);
+        cookieString = cookieString + "; expire=" + date.toGMTString();
+    }
+    document.cookie = cookieString;
 }
 
-function getCookie(name){
-             var strCookie=document.cookie;
-             var arrCookie=strCookie.split("; ");
-             for(var i=0;i<arrCookie.length;i++){
-                   var arr=arrCookie[i].split("=");
-                   if(arr[0]==name)return arr[1];
-             }
-             return "";
+function getCookie(name) {
+    var strCookie = document.cookie;
+    var arrCookie = strCookie.split("; ");
+    for (var i = 0; i < arrCookie.length; i++) {
+        var arr = arrCookie[i].split("=");
+        if (arr[0] == name) return arr[1];
+    }
+    return "";
 }
 
-function deleteCookie(name){
-              var date=new Date();
-              date.setTime(date.getTime()-10000);
-              document.cookie=name+"=v; expire="+date.toGMTString();
+function deleteCookie(name) {
+    var date = new Date();
+    date.setTime(date.getTime() - 10000);
+    document.cookie = name + "=v; expire=" + date.toGMTString();
 }
